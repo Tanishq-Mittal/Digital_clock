0b48f17c9981a73b4e3b243db82889f98ec27ebb
Tanishq Mittal <mittaltanishq469@gmail.com>
2026-06-22 12:17:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 st.title("🕒 Digital Clock")
 
-current_time = datetime.now().strftime("%H:%M:%S %p")
-current_date = datetime.now().strftime("%d/%m/%Y")
+india_time = datetime.now(ZoneInfo("Asia/Kolkata"))
 
-st.markdown(f"## {current_time}")
-st.markdown(f"### {current_date}")
+st.write("Current Time:")
+st.code(india_time.strftime("%H:%M:%S %p"))
+
+st.write("Current Date:")
+st.code(india_time.strftime("%d/%m/%Y"))
